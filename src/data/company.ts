fc25329229a3c0e6d66d6c4c1a434dfdd2a53610
Frank Matranga <thefrankmatranga@gmail.com>
2022-09-30 23:31:11 -0400

    Create layout and company data

---
+++ src/data/company.ts
@@ -0,0 +1,4 @@
+export const name = "Matranga Development";
+export const legalName = "Matranga Development"; // "Matranga Development, LLC"
+export const phoneNumber = "(212) 287-7845";
+export const tagLine = "Serving local businesses in the NY Capital Region";
