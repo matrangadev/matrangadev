023644f217593bce121569ba8338c014504a05f7
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-01 19:40:43 -0400

    Add footer columns

--- src/data/company.ts
+++ src/data/company.ts
@@ -2,3 +2,6 @@ export const name = "Matranga Development";
 export const legalName = "Matranga Development"; // "Matranga Development, LLC"
 export const phoneNumber = "(212) 287-7845";
 export const tagLine = "Serving local businesses in the NY Capital Region";
+export const email = "frank@matranga.dev";
+export const mission =
+  "We create software to help you run your business and save you time and money.";
