9f663e73d2582384c47e59a1da592f900c77c887
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-01 20:25:26 -0400

    Add prefetch plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
-
 import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.matranga.dev",
-  integrations: [compress(), sitemap()]
+  integrations: [compress(), sitemap(), prefetch()]
 });
