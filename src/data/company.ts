27f4be9eb3573c91be7aa804e383412c00a570cb
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-08 22:44:35 -0400

    Improve sizing on mobile

--- src/data/company.ts
+++ src/data/company.ts
@@ -1,7 +1,9 @@
 export const name = "Matranga Development";
 export const legalName = "Matranga Development"; // "Matranga Development, LLC"
 export const phoneNumber = "(212) 287-7845";
-export const tagLine = "Serving local businesses in the NY Capital Region";
+export const tagLine =
+  "Serving local businesses and nonprofits in the NY Capital Region";
+export const tagLinePrefix = "Serving local businesses and nonprofits in";
 export const email = "frank@matranga.dev";
 export const mission =
-  "We create software to help you run your business and save you time and money.";
+  "We create software to help you run your organization and save you time and money.";
