25d3ea0d3db3394a71a3480fbddd19b4d1ed2b0f
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-09 19:38:26 -0400

    Replace local with small

--- src/data/company.ts
+++ src/data/company.ts
@@ -2,8 +2,8 @@ export const name = "Matranga Development";
 export const legalName = "Matranga Development"; // "Matranga Development, LLC"
 export const phoneNumber = "(212) 287-7845";
 export const tagLine =
-  "Serving local businesses and nonprofits in the NY Capital Region";
-export const tagLinePrefix = "Serving local businesses and nonprofits in";
+  "Serving small businesses and nonprofits in the NY Capital Region";
+export const tagLinePrefix = "Serving small businesses and nonprofits in";
 export const email = "frank@matranga.dev";
 export const mission =
   "We create software to help you run your organization and save you time and money.";
