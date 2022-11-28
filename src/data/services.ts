42e692192bec909d3f38b479178d03b4cf5d1f4a
Frank Matranga <thefrankmatranga@gmail.com>
2022-11-27 22:44:36 -0500

    Cut down services

--- src/data/services.ts
+++ src/data/services.ts
@@ -6,21 +6,15 @@ interface Service {
 
 export const services: Service[] = [
   {
-    name: "Website Development",
-    iconName: "ic:round-web",
+    name: "Custom Software Development",
+    iconName: "ant-design:tool-filled",
     description:
-      "We can create a straightforward informational website for your organization. We'll ensure these are fast, secure, and accessible.",
+      "We can create a custom application to manage your data, keep track of customers, aid in fundraising, or serve any business need. We create these uniquely for you, instead of trying to retrofit existing software.",
   },
   {
     name: "Mobile App Development",
     iconName: "bi:phone-fill",
     description:
-      "We can create a custom phone app for your organization in the Android or iOS App Store. We'll ensure these are interactive and user-friendly.",
-  },
-  {
-    name: "Custom Software Development",
-    iconName: "ant-design:tool-filled",
-    description:
-      "We can create a custom application to manage your data, keep track of customers, aid in fundraising, or whatever your business needs are.",
+      "We can create a custom phone app for your organization in the Android or iOS App Store. We'll ensure these are interactive and user-friendly. These can be for yourself and your associates, and/or your customers.",
   },
 ];
