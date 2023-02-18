37abfc2d97f1c51ff617a19284fa703bdcf5b5fa
Frank Matranga <thefrankmatranga@gmail.com>
2023-02-18 17:50:59 -0500

    Service revamp

--- src/data/company.ts
+++ src/data/company.ts
@@ -2,8 +2,9 @@ export const name = "Matranga Development";
 export const legalName = "Matranga Development"; // "Matranga Development, LLC"
 export const phoneNumber = "(212) 287-7845";
 export const tagLine =
-  "Serving small businesses and nonprofits in the NY Capital Region";
-export const tagLinePrefix = "Serving small businesses and nonprofits in";
+  "Serving small families, small businesses, and nonprofits in the NY Capital Region";
+export const tagLinePrefix =
+  "Serving families, small businesses, and nonprofits in";
 export const email = "frank@matranga.dev";
 export const mission =
   "Giving back to the community by creating software to save your organization time and money.";
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,26 +1,36 @@
-interface Service {
+type Service = {
   name: string;
-  iconName: string;
   description: string;
-}
+  mediaAlt: string;
+} & (
+  | {
+      imageURL: string;
+    }
+  | {
+      videoURL: string;
+    }
+);
 
 export const services: Service[] = [
   {
+    name: "Technology Consulting",
+    imageURL: "/assets/man_with_questions.svg",
+    mediaAlt: "Smart phone held in a hand",
+    description:
+      "We can advise you on any technology choices you need to make for your organization. We can perform research and make suggestions or take a more active role in helping you implement whatever you need, e.g. Salesforce, Shopify, Wordpress, Stripe, etc.",
+  },
+  {
     name: "Software Development",
-    iconName: "ant-design:tool-filled",
+    imageURL: "/assets/phone_with_hand.svg",
+    mediaAlt: "Smart phone held in a hand",
     description:
       "We can create a custom application to manage your data, keep track of customers, aid in fundraising, or serve any business need. We create these uniquely for you, instead of trying to retrofit existing software.",
   },
   {
-    name: "Mobile App Development",
-    iconName: "bi:phone-fill",
+    name: "Computer Science Education",
+    videoURL: "/assets/person_typing.mp4",
+    mediaAlt: "Smart phone held in a hand",
     description:
-      "We can create a custom phone app for your organization in the Android or iOS App Store. We'll ensure these are interactive and user-friendly. These can be for yourself and your associates, and/or your customers.",
-  },
-  {
-    name: "Information Technology Consulting",
-    iconName: "mdi:account-help",
-    description:
-      "We can advise you on any technology choices you need to make for your organization. We can perform research and make suggestions or take a more active role in helping you implement whatever you need, e.g. Salesforce, Shopify, Wordpress, Stripe, etc.",
+      "We can create a custom application to manage your data, keep track of customers, aid in fundraising, or serve any business need. We create these uniquely for you, instead of trying to retrofit existing software.",
   },
 ];
