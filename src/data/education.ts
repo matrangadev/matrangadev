e31b32ed34cdb92cbddee9050880d5e0566d4c84
Frank Matranga <thefrankmatranga@gmail.com>
2023-03-10 16:21:17 -0500

    Education pricing

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,6 +2,41 @@ export type Topic = {
   name: string;
 };
 
+export type Price = {
+  name: string;
+  dollarsPerHour: number;
+};
+
+export const educationPrivatePrices: Price[] = [
+  {
+    name: "Below Middle School",
+    dollarsPerHour: 50,
+  },
+  {
+    name: "Middle School",
+    dollarsPerHour: 60,
+  },
+  {
+    name: "High School and above",
+    dollarsPerHour: 70,
+  },
+];
+
+export const educationGroupPrices: Price[] = [
+  {
+    name: "Below Middle School",
+    dollarsPerHour: 25,
+  },
+  {
+    name: "Middle School",
+    dollarsPerHour: 30,
+  },
+  {
+    name: "High School and above",
+    dollarsPerHour: 35,
+  },
+];
+
 export const topicRows: Topic[][] = [
   [
     {
