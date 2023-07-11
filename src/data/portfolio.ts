981a06ce6e0a7d6943e9d3c5b1f571e2eefc5ff7
Frank Matranga <thefrankmatranga@gmail.com>
2023-07-11 01:52:59 -0400

    Hidden portfolio

---
+++ src/data/portfolio.ts
@@ -0,0 +1,83 @@
+type PortfolioExample = {
+  title: string;
+  subtitle: string;
+  description: string;
+  videoURL: string;
+  videoAlt: string;
+  href: string;
+};
+
+export const examples: PortfolioExample[] = [
+  {
+    title: "PCC Fundraising",
+    subtitle: "Virtual, themed fundraisers for Preston Center of Compassion",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid laboriosam rem hic reiciendis iste quia magnam ex, neque quas quis voluptate quod officiis odio omnis libero explicabo doloribus officia voluptates ea sit repellat minima commodi optio. Fugit repudiandae hic dicta voluptatum possimus aut, architecto ipsum doloribus voluptate similique! Expedita, cumque.",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "PCC Website",
+    subtitle: "Informational Wordpress site for Preston Center of Compassion",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid laboriosam rem hic reiciendis iste quia magnam ex, neque quas quis voluptate quod officiis odio omnis libero explicabo doloribus officia voluptates ea sit repellat minima commodi optio. Fugit repudiandae hic dicta voluptatum possimus aut, architecto ipsum doloribus voluptate similique! Expedita, cumque.",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "RCOS IO",
+    subtitle: "Administrative platform for the Rensselaer Center for Open Source.",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "Rathskeller Game Night",
+    subtitle: "Realtime, collaborative event RSVP and activity vote system",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "Wednesday at 9PM",
+    subtitle: "Open source podcast host and informational website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "LATE",
+    subtitle: "Collaborative web app to manage RPI students coursework, exams, schedules, study guides, etc.",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "RPI Accepted Student Matcher",
+    subtitle: "Realtime web app matching prospective RPI students with current students for 1-on-1 chats",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "ITWS Discord Bot",
+    subtitle: "Discord bot application and student verification portal",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+  {
+    title: "DTools",
+    subtitle: "Lightning fast image manipulation tools right in your browser",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio odit, harum iusto, voluptatum ducimus error eligendi provident unde consequuntur voluptas! Nobis eius ut sit tempora. Minus nesciunt dolore optio deleniti adipisci quos iure fugiat? Repellendus accusamus labore ad dolore rem, magnam nemo beatae quibusdam tempore porro, impedit reprehenderit quod?",
+    videoAlt: "",
+    videoURL: "",
+    href: "",
+  },
+];
