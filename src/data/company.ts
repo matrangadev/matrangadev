0554bd0b90e1825adf57e69eb392989623d695a0
Frank Matranga <thefrankmatranga@gmail.com>
2023-07-10 23:19:36 -0400

    Refactoring and icons

--- src/data/company.ts
+++ src/data/company.ts
@@ -2,7 +2,7 @@ export const name = "Matranga Development";
 export const legalName = "Matranga Development";
 export const phoneNumber = "(347)" + " 851" + "-" + "9465";
 export const tagLine =
-  "Serving small small businesses and nonprofits in the NY Capital Region";
-export const tagLinePrefix = "Serving small businesses and nonprofits in";
+  "Serving nonprofits and small businesses in the NY Capital Region";
+export const tagLinePrefix = "Serving nonprofits and small businesses in";
 export const email = "frank@matranga.dev";
-export const mission = "Giving back to the community through technology.";
+export const mission = "Giving back to my community through technology.";
