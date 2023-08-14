c53e1e79879ae02726394fd179ec168a9a7b0e50
Frank Matranga <thefrankmatranga@gmail.com>
2023-08-14 16:14:07 -0400

    Fix missing space

--- src/data/company.ts
+++ src/data/company.ts
@@ -3,6 +3,6 @@ export const legalName = "Matranga Development";
 export const phoneNumber = "(347)" + " 851" + "-" + "9465";
 export const tagLine =
   "Serving nonprofits and small businesses in the NY Capital Region";
-export const tagLinePrefix = "Serving nonprofits and small businesses in";
+export const tagLinePrefix = "Serving nonprofits and small businesses in ";
 export const email = "frank@matranga.dev";
 export const mission = "Giving back to my community through technology.";
